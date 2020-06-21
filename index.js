5926dde1b5df1169d0ab21b252fee20172720da8
mmjj-rgb <60490029+mmjj-rgb@users.noreply.github.com>
2020-06-21 13:18:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
 const Discord = require('discord.js');
-
+const config = require('./config.json');
 const client = new Discord.Client();
 
-const prefix = require('./config.json');
-
-const embed = new Discord.RichEmbed()
-
-client.on('ready', () => {
- 	console.log('Ready!');
+client.once('ready', () => {
+	console.log('Ready!');
 });
-
- 
 
 client.on('message', message => {
-    if (message.content === 'config.prefixping') {
-
-       message.reply('pong');
-
-       } else if (message.content === 'xd') {
-
-           message.reply(':banana:');
-
-       }
-
-
+	if (message.content === '!ping') {
+		message.channel.send('Pong.');
+	}
 });
-
- 
-
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
