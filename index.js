fda7d598d5209eeda8881a94f760d4180271d564
mmjj-rgb <60490029+mmjj-rgb@users.noreply.github.com>
2020-06-21 14:20:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,25 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	if (message.content === `!ping`) {
-		message.channel.send('Pong.');
-	} else if (message.content === `xd`) {
-		message.channel.send('xd');
-	}
+	if (message.content === `!embed`) {
+            const embed = new Discord.RichEmbed()
+            .setTitle("This is your title, it can hold 256 characters")
+            .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+            .setColor(0x00AE86)
+            .setDescription("This is the main body of text, it can hold 2048 characters.")
+            .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+            .setImage("http://i.imgur.com/yVpymuV.png")
+            .setThumbnail("http://i.imgur.com/p2qNFag.png")
+            .setTimestamp()
+            .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+            .addField("This is a field title, it can hold 256 characters",
+            "This is a field value, it can hold 1024 characters.")
+            .addField("Inline Field", "They can also be inline.", true)
+            .addBlankField(true)
+            .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+ 
+  message.channel.send({embed});
+}
 
 });
 
